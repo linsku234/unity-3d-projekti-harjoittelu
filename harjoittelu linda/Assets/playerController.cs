9ba1cc0d4576001f099d1a1d4febda0be5f5a629
Your Name <you@example.com>
2023-11-21 18:15:37 +0200

    liikkumis

---
+++ harjoittelu linda/Assets/playerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+   public float moveSpeed = 4.5f;
+   public CharacterController controller;
+
+   Vector3 moveDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        moveDir = new Vector3(horizontal,0,vertical);
+
+        controller.SimpleMove(moveDir * moveSpeed);
+    }
+}
