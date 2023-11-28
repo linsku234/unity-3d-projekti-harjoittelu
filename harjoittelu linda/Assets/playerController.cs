a4e451691370038d8966326828a6bf536a0b16de
Your Name <you@example.com>
2023-11-28 18:09:44 +0200

    kameran kääntely

--- harjoittelu linda/Assets/playerController.cs
+++ harjoittelu linda/Assets/playerController.cs
@@ -8,11 +8,18 @@ public class playerController : MonoBehaviour
    public float moveSpeed = 4.5f;
    public CharacterController controller;
 
+    public float mouseSpeed = 3f;
+    public CursorLockMode cursorLock;
+    private float mouseMovementX = 0f;
+
+    private float mouseMovementY = 0f;
+
    Vector3 moveDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = cursorLock;
+        Cursor.visible = (CursorLockMode.Locked != cursorLock);
     }
 
     // Update is called once per frame
@@ -20,8 +27,18 @@ public class playerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        mouseMovementX += Input.GetAxis("Mouse X") * mouseSpeed;
+        mouseMovementY -= Input.GetAxis("Mouse Y") * mouseSpeed;
+
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseMovementY, 0, 0);
+
+        transform.localRotation = Quaternion.Euler(0,mouseMovementX,0);
+        
         moveDir = new Vector3(horizontal,0,vertical);
+        moveDir = transform.rotation * moveDir;
 
         controller.SimpleMove(moveDir * moveSpeed);
+
     }
 }
